3f4e602764676d704b0fcb20ecbbc214fc97f6e3
Zsyrhill <zsyrhillsoria@gmail.com>
2022-03-05 21:44:35 +0800

    version 0.1

--- sidebar.php
+++ sidebar.php
@@ -58,6 +58,9 @@
                         </a>
                         <ul class="collapse nav flex-column ms-4" id="submenu2" data-bs-parent="#menu">
                             <li class="w-100">
+                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">New</span> Doctor</a>
+                            </li>
+                            <li>
                                 <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">View</span> Doctors</a>
                             </li>
                             <li>
