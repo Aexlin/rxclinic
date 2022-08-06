bcf5bce7c0059995b1be8b0c8d203dd46dbb88c9
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2022-08-06 20:56:25 +0800

    css refactor

---
+++ includes/imports.php
@@ -0,0 +1,46 @@
+<html>
+<? // Stylesheet imports ?>
+<head>
+<link type="text/css" href="assets/vendor/simplebar.min.css" rel="stylesheet"><!-- App CSS -->
+<link type="text/css" href="assets/css/app.css" rel="stylesheet">
+<link type="text/css" href="assets/css/app.rtl.css" rel="stylesheet"><!-- Material Design Icons -->
+<link type="text/css" href="assets/css/vendor-material-icons.css" rel="stylesheet">
+<link type="text/css" href="assets/css/vendor-material-icons.rtl.css" rel="stylesheet">
+<!-- Font Awesome FREE Icons -->
+<link type="text/css" href="assets/css/vendor-fontawesome-free.css" rel="stylesheet">
+<link type="text/css" href="assets/css/vendor-fontawesome-free.rtl.css" rel="stylesheet"><!-- ion Range Slider -->
+<link type="text/css" href="assets/css/vendor-ion-rangeslider.css" rel="stylesheet">
+<link type="text/css" href="assets/css/vendor-ion-rangeslider.rtl.css" rel="stylesheet"><!-- Flatpickr -->
+<link type="text/css" href="assets/css/vendor-flatpickr.css" rel="stylesheet">
+<link type="text/css" href="assets/css/vendor-flatpickr.rtl.css" rel="stylesheet">
+<link type="text/css" href="assets/css/vendor-flatpickr-airbnb.css" rel="stylesheet">
+<link type="text/css" href="assets/css/vendor-flatpickr-airbnb.rtl.css" rel="stylesheet"><!-- Vector Maps -->
+<link type="text/css" href="assets/vendor/jqvmap/jqvmap.min.css" rel="stylesheet">
+</head>
+<? // Script imports ?>
+    <!-- jQuery -->
+    <script src="assets/vendor/jquery.min.js"></script><!-- Bootstrap -->
+    <script src="assets/vendor/popper.min.js"></script>
+    <script src="assets/vendor/bootstrap.min.js"></script><!-- Simplebar -->
+    <script src="assets/vendor/simplebar.min.js"></script><!-- DOM Factory -->
+    <script src="assets/vendor/dom-factory.js"></script><!-- MDK -->
+    <script src="assets/vendor/material-design-kit.js"></script><!-- Range Slider -->
+    <script src="assets/vendor/ion.rangeSlider.min.js"></script>
+    <script src="assets/js/ion-rangeslider.js"></script><!-- App -->
+    <script src="assets/js/toggle-check-all.js"></script>
+    <script src="assets/js/check-selected-row.js"></script>
+    <script src="assets/js/dropdown.js"></script>
+    <script src="assets/js/sidebar-mini.js"></script>
+    <script src="assets/js/app.js"></script><!--App Settings(safe to remove)-->
+    <script src="assets/js/app-settings.js"></script><!-- Flatpickr -->
+    <script src="assets/vendor/flatpickr/flatpickr.min.js"></script>
+    <script src="assets/js/flatpickr.js"></script><!-- Global Settings -->
+    <script src="assets/js/settings.js"></script><!-- Chart.js -->
+    <script src="assets/vendor/Chart.min.js"></script><!-- App Charts JS -->
+    <script src="assets/js/chartjs-rounded-bar.js"></script>
+    <script src="assets/js/charts.js"></script><!-- Vector Maps -->
+    <script src="assets/vendor/jqvmap/jquery.vmap.min.js"></script>
+    <script src="assets/vendor/jqvmap/maps/jquery.vmap.world.js"></script>
+    <script src="assets/js/vector-maps.js"></script><!-- Chart Samples -->
+    <script src="assets/js/page.dashboard.js"></script>
+</html>
